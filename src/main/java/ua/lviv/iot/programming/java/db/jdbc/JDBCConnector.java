f6e256e83f0d5a2dc899c41b7a4195165e7ed1ff
Zenoviy Veres <viruslviv@gmail.com>
2017-03-20 00:19:07 +0200

    Initialize connection to mySQL DB and create test to check that Student is returned from DB

--- src/main/java/ua/lviv/iot/programming/java/db/jdbc/JDBCConnector.java
+++ src/main/java/ua/lviv/iot/programming/java/db/jdbc/JDBCConnector.java
@@ -1,12 +1,20 @@
 package ua.lviv.iot.programming.java.db.jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 import ua.lviv.iot.programming.java.db.jdbc.model.Student;
 
 public class JDBCConnector {
 
-    
-    //private static Connector
     public Student getStudent() {
+        try (Connection con = DriverManager.getConnection("jdbc:myDriver:mySql", "viruslviv", "viruslviv")) {
+            con.createStatement();
+
+        } catch (Exception exception) {
+
+        }
+
         return null;
     }
 }
--- src/test/java/ua/lviv/iot/programming/java/db/jdbc/JDBCConnectorTest.java
+++ src/test/java/ua/lviv/iot/programming/java/db/jdbc/JDBCConnectorTest.java
@@ -1,5 +1,14 @@
 package ua.lviv.iot.programming.java.db.jdbc;
 
+import org.junit.Test;
+
+import org.junit.Assert;
+
 public class JDBCConnectorTest {
 
+    @Test
+    public void testGetStudent() {
+        JDBCConnector connector = new JDBCConnector();
+        Assert.assertNotNull(connector.getStudent());
+    }
 }
